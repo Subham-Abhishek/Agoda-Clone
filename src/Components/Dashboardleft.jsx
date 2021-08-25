00f4140ae61e539b31fa9e492ddb0c0466ce7046
Milind <milindanand74@gmail.com>
2021-08-26 04:44:57 +0530

    done

--- src/Components/Dashboardleft.jsx
+++ src/Components/Dashboardleft.jsx
@@ -1,18 +1,82 @@
 import React from 'react'
 import styles from "./dashboardleft.module.css";
+import Checkbox from '@material-ui/core/Checkbox';
 
-import Paper from "@material-ui/core/Paper";
 
 const Dashboardleft = () => {
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChangecheck = (event) => {
+      setChecked(event.target.checked);
+    };
+  
+    
     return (
        <>
-<div>
+
+<div className={styles.checkdiv} >
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWzJasTE1hLoKULMQCJ1HYIgQpyXjLVEtzqK02jGp0KUvwXftgCp8Cj1zdLjPa-Jhv_E&usqp=CAU" alt="" />
-    </div>       
-       <div>
+    </div>   
+    <div  className={styles.checkdiv}>
+
+        
+       <div >
            <p>Distance to center</p>
-           
-       </div>
+    <span> <Checkbox
+        // checked={checked}
+        color="primary"
+        onChange={handleChangecheck}
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      Inside city center(469)
+    </span>
+</div>
+<div>
+
+    <span> <Checkbox
+        // checked={checked}
+        color="primary"
+        onChange={handleChangecheck}
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+    2 km to center (159)
+    </span>
+    </div>
+<div>
+    <span> <Checkbox
+        // checked={checked}
+        color="primary"
+        onChange={handleChangecheck}
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+          2-5km to center (131)
+
+    </span>
+    </div>
+
+<div>
+        <span> <Checkbox
+                // checked={checked}
+                color="primary"
+                onChange={handleChangecheck}
+                inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+                      7km to center (201)
+
+            </span>
+        </div>
+    
+     <span> <Checkbox
+        // checked={checked}
+        color="primary"
+        onChange={handleChangecheck}
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+                10km to center (151)
+
+    </span>
+    </div>
+
        </>
     )
 }
