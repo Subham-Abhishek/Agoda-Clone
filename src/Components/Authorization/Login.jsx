3722e4ee0997f0ddc68efb0c9f78bd7658221571
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-08-25 21:14:58 +0530

    added login page

---
+++ src/Components/Authorization/Login.jsx
@@ -0,0 +1,95 @@
+import { Typography, FormControlLabel, Checkbox, withStyles } from '@material-ui/core'
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import "./Auth-page.css"
+import { Loading } from './Loading'
+import GoogleLogin from 'react-google-login'
+import { FaApple } from "react-icons/fa";
+import { SiFacebook } from "react-icons/si";
+
+
+
+export const Register = () => {
+    const [isLoading, setLoading] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        let timer = setTimeout(() => {
+            setLoading(false)
+        }, 2000);
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [])
+
+
+    function responseGoogle(response) {
+        console.log(response);
+        console.log(response.profileObj);
+    }
+    return (
+        <div className="resister-page">
+            {isLoading ? <Loading /> :
+                <div className="signup">
+                    <Typography>
+                        Sign in
+                    </Typography>
+                    <div className="mode-switch">
+                        <div className="auth-active">
+                            EMAIL
+                        </div>
+                        <div className="auth-disabled">
+                            MOBILE
+                        </div>
+                    </div>
+                   
+                    <div className="inputs">
+                        <label>Email</label>
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <div className="inputs">
+                        <label>Password</label>
+                        <input type="password" placeholder="First Name" />
+                    </div>
+                   
+                    <div className="confirm">
+                        <FormControlLabel
+                            control={<Checkbox value="allowExtraEmails" color="primary" />}
+                            label="Email me exclusive Agoda promotions.  I can opt out later as stated in the Privacy Policy."
+                        />
+                    </div>
+                   
+                    <button className="signup-btn">Sign Up</button>
+                    <div className="break">
+                        <div className="line"></div>
+                        <p>or continue with</p>
+                        <div className="line"></div>
+                    </div>
+                    <div className="buttons" style={{ border: "2px solid #a6a6a6" }} variant="outlined">
+                        {/* <GoogleLogin
+                            clientId="979909697763-pe6gr2hbnarqpvdj31fh3ak86gfacg7a.apps.googleusercontent.com"
+                            buttonText="Sign in with Google"
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            cookiePolicy={'single_host_origin'}
+
+                        /> */}
+                    </div>
+                    <div className="social">
+                        <div className="buttons" style={{ border: "2px solid black" }} variant="outlined">
+                            <FaApple style={{ marginRight: "90px", width: 25, height: 25 }} /> Sign in with Apple
+                        </div>
+
+                        <div className="buttons" style={{ border: "2px solid #1877f2", color: "#1877f2" }} variant="outlined">
+                            <SiFacebook style={{ marginRight: "75px", width: 25, height: 25 }} /> Sign in with Facebook
+                        </div>
+                    </div>
+                    <div className="line1"></div>
+                    <div className="buttons">Already have an account? Sign in</div>
+                    <p>By signing in, I agree to Agoda's Terms of Use and Privacy Policy.</p>
+                </div>
+
+            }
+        </div>
+    )
+}
