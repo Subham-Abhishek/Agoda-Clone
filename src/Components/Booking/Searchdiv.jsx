486e029f2524f373d1ffe9ee8528e5377978d0ab
Milind <milindanand74@gmail.com>
2021-08-27 04:21:18 +0530

    done

--- src/Components/Booking/Searchdiv.jsx
+++ src/Components/Booking/Searchdiv.jsx
@@ -4,8 +4,9 @@ import styles from "./Sortingdiv.module.css";
 import {Paper,Button,InputBase,IconButton} from "@material-ui/core";
 // import SearchIcon from '@material-ui/icons/Search';
 import GroupIcon from '@material-ui/icons/Group';
-
+import SearchIcon from '@material-ui/icons/Search';
 import { searchData } from "../../Bookingreducer/Redux/action";
+import TextField from '@material-ui/core/TextField';
 
 const Searchdiv = () => {
     const [query,setQuery]=useState("")
@@ -23,10 +24,10 @@ const Searchdiv = () => {
         onChange={(e)=>setQuery(e.target.value)} placeholder="search"/> */}
  
     <div>
-      
+    <SearchIcon className={styles.publicicon1}/>
 <InputBase  className={styles.searchinput} value={query}  
    onChange={(e)=>setQuery(e.target.value)}
-              placeholder="Search"
+              placeholder="        Enter a destination or property"
               
               inputProps={{ 'aria-label': 'search' }}
             />
@@ -41,15 +42,16 @@ const Searchdiv = () => {
     defaultValue="2017-05-24"         
    inputProps={{ 'aria-label': 'search' }}
  />
-< GroupIcon className={styles.publicicon} >
-
-</GroupIcon>
+  
+  < GroupIcon className={styles.publicicon} />
 
 <InputBase  className={styles.searchinputroom} 
    inputProps={{ 'aria-label': 'search' }}
 
-   defaultValue="2 rooms"
- />
+   defaultValue="        2 rooms"
+ >
+ </InputBase>
+
 
 
         <button variant="contained"  className={styles.searchbtn}  onClick={handlesearch}>Search</button>
