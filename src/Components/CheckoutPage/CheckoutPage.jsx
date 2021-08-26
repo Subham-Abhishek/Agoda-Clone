b8f5353f8c8835c57b306c139d0fe4d052a10a58
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-08-26 13:07:57 +0530

    added reminder loader

--- src/Components/CheckoutPage/CheckoutPage.jsx
+++ src/Components/CheckoutPage/CheckoutPage.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import "../Authorization/Auth-page.css";
 import { CheckoutLoading } from './CheckoutLoading';
+import { ConfirmLoading } from './ConfirmLoading';
 export const CheckoutPage = () => {
     const [isLoading, setLoading] = useState(false);
+    const [isremind, setRemind] = useState(false);
 
     useEffect(() => {
       setLoading(true);
       let timer = setTimeout(() => {
-        setLoading(true);
+        setLoading(false);
       }, 2000);
+      let timer2 = setTimeout(() => {
+        setRemind(true);
+      }, 8000);
       return () => {
         clearTimeout(timer);
+        clearTimeout(timer2);
       };
     }, []);
 
 
     
     return isLoading ? <CheckoutLoading/> :
-       ( <div>
+       (
+         <> 
+        <div>
             sdsd
         </div>
+        {isremind && <ConfirmLoading setRemind={setRemind}/>}
+        </>
     )
 }
---
+++ src/Components/CheckoutPage/ConfirmLoading.jsx
@@ -0,0 +1,18 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+
+export const ConfirmLoading = ({setRemind}) => {
+  return (
+    <div className="check-loading">
+      <div className="loader-container2">
+        <img className="remind-loader" src="remind.png" alt="" />
+        <span class="remind-topic">You're almost done!</span>
+        <span class="remind-info">
+          <span className="important">Our last room for your dates! </span>
+           if you leave now, we can't hold this room and rate for you.
+        </span>
+        <button onClick={()=>setRemind(false)} className="continue">Continue booking</button>
+      </div>
+    </div>
+  );
+};
