e27ddbf4c396627860cd3e9e33e66a5e736007c8
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-08-26 21:00:46 +0530

    added step progress

--- src/Components/CheckoutPage/CheckoutPage.jsx
+++ src/Components/CheckoutPage/CheckoutPage.jsx
@@ -1,34 +1,54 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import "../Authorization/Auth-page.css";
-import { CheckoutLoading } from './CheckoutLoading';
-import { ConfirmLoading } from './ConfirmLoading';
+import "./Checkout.css"
+import { CheckoutLoading } from "./CheckoutLoading";
+import { ConfirmLoading } from "./ConfirmLoading";
+import { ProgressBar, Step } from "react-step-progress-bar";
+import { FaCheck} from "react-icons/fa";
+import CountdownTimer from "react-component-countdown-timer";
+import { Timer } from "./Timer";
+
 export const CheckoutPage = () => {
-    const [isLoading, setLoading] = useState(false);
-    const [isremind, setRemind] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [isremind, setRemind] = useState(false);
+  const [step1, setStep1] = useState(true);
+  const [step2, setStep2] = useState(false);
+  const [step3, setStep3] = useState(false);
+  const [activeStep, setActiveStep] = React.useState(0);
 
-    useEffect(() => {
-      setLoading(true);
-      let timer = setTimeout(() => {
-        setLoading(false);
-      }, 2000);
-      let timer2 = setTimeout(() => {
-        setRemind(true);
-      }, 8000);
-      return () => {
-        clearTimeout(timer);
-        clearTimeout(timer2);
-      };
-    }, []);
+ 
 
-
-    
-    return isLoading ? <CheckoutLoading/> :
-       (
-         <> 
-        <div>
-            sdsd
+  return isLoading ? (
+    <CheckoutLoading />
+  ) : (
+    <div className="checkout-Wrapper">
+      <nav>
+        <img src="agoda-logo.png" alt="" />
+        <div className="progress">
+          <div className="progress-bar">
+            <div className="step1">
+              <span className={step1?"sub-active":"sub-deactive"} >1</span>
+              <div className={step1?"progress-info-active":"progress-info-deactive"}>Customer information</div>
+            </div>
+            <div className="step2">
+              <span className={step2?"sub-active":"sub-deactive"} >2</span>
+              <div className={step2?"progress-info-active":"progress-info-deactive"}>Payment information</div>
+            </div>
+            <div className="step3">
+              <span className={step3?"sub-active":"sub-deactive"}><FaCheck style={{width:8}}/></span>
+              <div className={step3?"progress-info-active":"progress-info-deactive"}>Booking is confirmed</div>
+            </div>
+          </div>
         </div>
-        {isremind && <ConfirmLoading setRemind={setRemind}/>}
-        </>
-    )
-}
+      </nav>
+      
+      <div className='checkout-container'>
+        <div className="userInfo">
+        <Timer/>
+       
+        </div>
+        <div className="hotel-detail"></div>
+      </div>
+    </div>
+  );
+};
---
+++ src/Components/CheckoutPage/Timer.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+
+
+export function Timer() {
+	const [hours, setHours] = useState(0);
+	const [minutes, setMinutes] = useState(0);
+	const [seconds, setSeconds] = useState(0);
+	const [showSemicolon, setShowSemicolon] = useState(false);
+
+	useEffect(() => {
+		setInterval(() => {
+			const now = moment();
+			const then = moment('2020-09-26 18:59:12', 'YYYY-MM-DD hh:mm:ss');
+			const countdown = moment(then - now);
+			setHours(countdown.format('HH'));
+			setMinutes(countdown.format('mm'));
+			setSeconds(countdown.format('ss'));
+		}, 1000);
+	}, []);
+
+	return (
+		<div className="app">
+		
+			<div className="timer-container">
+				
+				{showSemicolon ? <div className="semicolon">:</div> : null}
+				<div className="timer">
+					{hours}
+		
+				</div>
+				{showSemicolon ? <div className="semicolon">:</div> : null}
+				<div className="timer">
+					{minutes}
+			
+				</div>
+				{showSemicolon ? <div className="semicolon">:</div> : null}
+				<div className="timer">
+					{seconds}
+				
+				</div>
+			</div>
+		</div>
+	);
+}
+
