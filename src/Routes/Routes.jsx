0ed49d0ad879a41d47c5603a6cafad8d9188e34b
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-08-28 15:54:50 +0530

    Authentication done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,14 +3,11 @@ import { Route, Switch } from "react-router-dom";
 import { Login } from "../Components/Authorization/Login";
 import { Register } from "../Components/Authorization/Register";
 import { CheckoutPage } from "../Components/CheckoutPage/CheckoutPage";
-import Landing from "../Components/Landing-page/Landing";
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route exact path="/">
-        <Landing />
-      </Route>
+      
       <Route path="/register">
         <Register />
       </Route>
