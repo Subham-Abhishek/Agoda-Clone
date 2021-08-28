3b788d7840829173bfd3028bc20092a37099e73a
Subham-Abhishek <abhi950727@gmail.com>
2021-08-28 23:59:13 +0530

    action in redux updated

--- src/Bookingreducer/Redux/action.js
+++ src/Bookingreducer/Redux/action.js
@@ -68,17 +68,3 @@ export const searchDataFailure = (err) => {
         payload: err
     }
 }
-
-// export const searchData = (payload) => dispatch => {
-//     console.log("ok:" ,payload.toLowerCase());
-//     dispatch(searchDataRequest())
-//     return axios.get(`http://localhost:3001/hotel?city=${payload.toLowerCase()}`)
-//         .then((res) => {
-//             // console.log(res.data)
-//             dispatch(searchDataSuccess(res.data))
-//         })
-//         .catch((err) => {
-//             dispatch(searchDataFailure(err))
-//         })
-
-// }
