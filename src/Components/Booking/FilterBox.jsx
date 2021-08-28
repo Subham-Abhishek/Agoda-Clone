65c3a9fab6fdf15d466e826da454a2fc0856c4aa
Milind <milindanand74@gmail.com>
2021-08-29 01:01:45 +0530

    filterbox chanh=genged

--- src/Components/Booking/FilterBox.jsx
+++ src/Components/Booking/FilterBox.jsx
@@ -12,18 +12,46 @@ const FilterBox = () => {
   const getUserDetails = () => {
     axios
       .get(`http://localhost:3001/hotel/${id}`)
+        
       .then((res) => {
         console.log(res.data);
         setUser(res.data);
+        postusers()
+
       })
       .catch((err) => {});
   };
+
+
+
   useEffect(() => {
     getUserDetails();
   }, []);
 
+  const postusers = ()=>{
+    axios.post("http://localhost:3035/selecthotel", user)
+    .then((res) => {
+      console.log( "postdata=",res.data);
+      gethotels()
+    })
+    .catch((err) => {});
+};
+
+
+const gethotels = ()=>{
+  axios.get("http://localhost:3035/selecthotel")
+  .then((res) => {
+    console.log( "getdata=",res.data);
+  
+  })
+  .catch((err) => {
+    console.log("error from gethotels=",err)
+  });
+};
+
+  console.log("user=", user)
   return (
-    <div>
+    <div >
       <button>Quarintine</button>
       <h3>{user.hotel}</h3>
       <p>dkdskldkldlkdkllk</p>
